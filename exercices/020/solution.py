82cf390709f1db63950f322b4dafeb6bde687a33
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-22 16:47:07 +0200

    ex020

--- exercices/020/solution.py
+++ exercices/020/solution.py
@@ -7,6 +7,6 @@ Created on Mon Sep 22 16:04:20 2014
 
 import sys
 if len(sys.argv) < 3:
-    print("you must give two arguments")
+    print("usage: python3 solution.py OP1 OP2")
 else:
     print(int(sys.argv[1]) - int(sys.argv[2]))
