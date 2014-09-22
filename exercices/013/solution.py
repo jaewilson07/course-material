c1dbaa2903b0e614e1b0b2bcd66468beac56b0d5
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-22 15:39:13 +0200

    ex013

---
+++ exercices/013/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 15:05:29 2014
+
+@author: Catherine
+"""
+
+import sys
+if len(sys.argv) == 1:
+    print("You must enter a parameter.")
+else:
+    print(sys.argv[1])
