e5cc73e773e8ce659ee6cb4f161b558048d68fd6
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-22 17:35:47 +0200

    ex070

---
+++ exercices/070/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 17:24:00 2014
+
+@author: Catherine
+"""
+
+for i in range(97, 123):
+    for j in range(97, 123):
+        if i != j:
+            print(chr(i), chr(j))
