acaceafe2ff5ed28ae29ce9d29cf2d6aba98d4fe
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-22 18:02:44 +0200

    ex080

---
+++ exercices/080/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 17:37:44 2014
+
+@author: Catherine
+"""
+
+c = 0
+a = "abcdefghijklmnopqrstuvwxyz"
+for i in a:
+    c = c + 1
+    for j in a[c:26]:
+        print(i, j)
