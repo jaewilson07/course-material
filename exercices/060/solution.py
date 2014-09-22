f24e0a7b9ec7961132483497a660adde783ce3e8
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-22 17:31:15 +0200

    ex060

---
+++ exercices/060/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 17:24:00 2014
+
+@author: Catherine
+"""
+
+for i in range(97, 123):
+    for j in range(97, 123):
+        print(chr(i), chr(j))
