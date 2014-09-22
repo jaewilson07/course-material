4113ebd2a77827970c3c53fa05ecc015094e0e5f
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-22 15:18:54 +0200

    ex009

--- exercices/009/solution.py
+++ exercices/009/solution.py
@@ -13,4 +13,3 @@ print(len("""Turmoil has engulfed the Galactic Republic. The\
  endlessly debates this alarming chain of events, the Supreme\
  Chancellor has secretly dispatched two Jedi Knights, the guardians of\
  peace and justice in the galaxy, to settle the conflict""".split()))
- 
