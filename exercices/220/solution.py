9b460acc45fa711a07f3921a256a24f97ac853e7
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-23 21:20:31 +0200

    ex220

--- exercices/220/solution.py
+++ exercices/220/solution.py
@@ -5,6 +5,7 @@ Created on Tue Sep 23 17:16:31 2014
 @author: Catherine
 """
 
+
 def is_prime(n):
     x = True
     if n > 1:
@@ -12,7 +13,7 @@ def is_prime(n):
             if (n % i) == 0:
                 x = False
     return x
-    
+
 s = []
 for n in range(9999, 10051):
     if is_prime(n) is True:
