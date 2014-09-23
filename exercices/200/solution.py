c68fda7d3658ffdbd9329f1bcc8c07cee46e6b30
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-23 22:18:02 +0200

    ex200

--- exercices/200/solution.py
+++ exercices/200/solution.py
@@ -8,7 +8,9 @@ Created on Tue Sep 23 14:57:07 2014
 
 def is_prime(n):
     x = True
-    if n > 1:
+    if n <= 1:
+        x = False
+    elif n > 1:
         for i in range(2, int(n ** .5)):
             if (n % i) == 0:
                 x = False
