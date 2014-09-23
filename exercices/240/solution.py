e079f93274de9d4b8ad18a70e39cb0c58ea794d8
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-23 19:13:39 +0200

    ex240

---
+++ exercices/240/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 18:51:08 2014
+
+@author: Catherine
+"""
+
+
+a, b = 1, 2
+x = ['1']
+for i in range(9):
+    x.append(str(b))
+    a, b = b, a+b
+print(', '.join(x))
