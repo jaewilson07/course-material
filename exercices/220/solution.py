305bfd7180d8bd27d24388a7b680055d92a506f0
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-23 20:09:59 +0200

    ex220

--- exercices/220/solution.py
+++ exercices/220/solution.py
@@ -5,10 +5,16 @@ Created on Tue Sep 23 17:16:31 2014
 @author: Catherine
 """
 
-import is_prime
-
-x = []
+def is_prime(n):
+    x = True
+    if n > 1:
+        for i in range(2, int(n ** .5)):
+            if (n % i) == 0:
+                x = False
+    return x
+    
+s = []
 for n in range(9999, 10051):
-    if is_prime.is_prime(n) is True:
-        x.append(str(n))
-print(', '.join(x))
+    if is_prime(n) is True:
+        s.append(str(n))
+print(', '.join(s))
