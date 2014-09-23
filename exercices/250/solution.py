bfef64c19b8853f1e8084e5a6fd4ccfead3560fb
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-23 22:32:54 +0200

    ex250

--- exercices/250/solution.py
+++ exercices/250/solution.py
@@ -7,8 +7,8 @@ Created on Tue Sep 23 19:14:17 2014
 
 
 def draw_n_squares(n):
-    x = '+---+'*n + '\n' + '|   |'*n
+    x = '+---'*n + '+' + '\n' + '|   '*n + '|'
     for i in range(n-1):
-        x = x + '\n' + '+---+'*n + '\n' + '|   |'*n
-    x = x + '\n' + '+---+'*n
+        x = x + '\n' + '+---'*n + '+' + '\n' + '|   '*n + '|'
+    x = x + '\n' + '+---'*n + '+'
     return x
