f9c761d33fc1b5ad6246ce1ba8cd94315cb44254
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-23 15:38:45 +0200

    ex201

---
+++ exercices/201/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 15:17:00 2014
+
+@author: Catherine
+"""
+
+import string
+
+
+def is_alpha(input):
+    return (input in list(string.ascii_lowercase))
