c0aadc46b36e68ad64a19d2fe13d9d3a4499e1c4
Catherine Eisenhauer <caeisenhauer@gmail.com>
2014-09-23 20:11:59 +0200

    ex210

--- exercices/210/solution.py
+++ exercices/210/solution.py
@@ -5,10 +5,17 @@ Created on Tue Sep 23 16:57:34 2014
 @author: Catherine
 """
 
-import is_prime
 
+def is_prime(n):
+    x = True
+    if n > 1:
+        for i in range(2, int(n ** .5)):
+            if (n % i) == 0:
+                x = False
+    return x
+    
 x = 0
 for n in range(1000):
-    if is_prime.is_prime(n) is True:
+    if is_prime(n) is True:
         x = x + n
 print(x)
